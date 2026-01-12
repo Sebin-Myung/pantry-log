f0fe158d0ed76059acea9362f15d071de84dac38
Sebin-Myung <wrqewrq213@naver.com>
2026-01-12 15:58:17 +0900

    feat: AddIngredientPage에 IngredientForm 적용

--- app/ingredient/add.tsx
+++ app/ingredient/add.tsx
@@ -1,14 +1,29 @@
-import { Text, View } from "react-native";
+import { IngredientForm, IngredientFormState } from "@widgets";
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet } from "react-native";
 
 export default function AddIngredientPage() {
+  const [formState, setFormState] = useState<IngredientFormState>({
+    name: "",
+    brand: "",
+    purchaseSource: "",
+    purchaseDate: new Date(),
+    quantity: { amount: "" },
+  });
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <Text>add ingredient</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 0} // header 높이
+    >
+      <ScrollView contentContainerStyle={styles.container}>
+        <IngredientForm state={formState} setState={setFormState} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { padding: 16, paddingBottom: 60 },
+});
