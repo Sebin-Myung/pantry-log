2fdf395881fd759b3ff787f343378388b938bbac
Sebin-Myung <wrqewrq213@naver.com>
2026-01-13 21:18:43 +0900

    refactor: AddIngredientPage pages 폴더로 이동

--- app/ingredient/add.tsx
+++ app/ingredient/add.tsx
@@ -1,29 +1,5 @@
-import { IngredientForm, IngredientFormState } from "@widgets";
-import { useState } from "react";
-import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet } from "react-native";
+import { AddIngredientPage } from "@pages";
 
-export default function AddIngredientPage() {
-  const [formState, setFormState] = useState<IngredientFormState>({
-    name: "",
-    brand: "",
-    purchaseSource: "",
-    purchaseDate: new Date(),
-    quantity: { amount: "" },
-  });
-
-  return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 0} // header 높이
-    >
-      <ScrollView contentContainerStyle={styles.container}>
-        <IngredientForm state={formState} setState={setFormState} />
-      </ScrollView>
-    </KeyboardAvoidingView>
-  );
+export default function Page() {
+  return <AddIngredientPage />;
 }
-
-const styles = StyleSheet.create({
-  container: { padding: 16, paddingBottom: 60 },
-});
