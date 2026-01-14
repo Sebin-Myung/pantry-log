e0b42bf3a5029aa8c959ea8a3bc553a63087cdbc
Sebin-Myung <wrqewrq213@naver.com>
2026-01-14 17:28:32 +0900

    feat: editIngredientPage 존재하지 않는 id 처리 적용

--- features_3/edit-ingredient/model/useEditIngredient.ts
+++ features_3/edit-ingredient/model/useEditIngredient.ts
@@ -1,9 +1,12 @@
 import { Ingredient, ingredientStorage, useIngredientStore } from "@entities";
+import { useBackButton } from "@shared";
 import { useLayoutEffect, useState } from "react";
+import { Alert } from "react-native";
 
 export interface IUseEditIngredient extends Pick<Ingredient, "id"> {}
 
 export function useEditIngredient({ id }: IUseEditIngredient) {
+  const { goBack } = useBackButton();
   const updateIngredient = useIngredientStore((state) => state.update);
 
   const [item, setItem] = useState<Ingredient>();
@@ -16,6 +19,7 @@ export function useEditIngredient({ id }: IUseEditIngredient) {
     const storageIngredient = ingredientStorage.getIngredient(id);
 
     if (!storageIngredient) {
+      Alert.alert("알림", "존재하지 않는 재료입니다.", [{ text: "확인", style: "default", onPress: goBack }]);
       return;
     }
 
