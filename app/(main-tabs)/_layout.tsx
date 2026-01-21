1318feb054f50f624c235e7b08f896817c6e8fc3
Sebin-Myung <wrqewrq213@naver.com>
2026-01-21 16:19:04 +0900

    feat: main-tabs style 수정

--- app/(main-tabs)/_layout.tsx
+++ app/(main-tabs)/_layout.tsx
@@ -1,10 +1,28 @@
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { Tabs } from "expo-router";
 
 export default function MainTabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="(storage-location-tabs)" options={{ title: "재료" }} />
-      <Tabs.Screen name="cooking-record" options={{ title: "요리 기록" }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "white",
+        tabBarLabelStyle: { fontSize: 14 },
+      }}>
+      <Tabs.Screen
+        name="(storage-location-tabs)"
+        options={{
+          title: "재료",
+          tabBarIcon: ({ color }) => <FontAwesome5 name="carrot" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="cooking-record"
+        options={{
+          title: "요리 기록",
+          tabBarIcon: ({ color }) => <FontAwesome5 name="pencil-alt" size={24} color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
