fd24f43c6a4f264d9c9ba23fb1525f3777ea5f1a
Sebin-Myung <wrqewrq213@naver.com>
2026-01-23 22:03:36 +0900

    type: CookingRecord type 수정

--- entities_2/cookingRecord/model/types.ts
+++ entities_2/cookingRecord/model/types.ts
@@ -2,5 +2,7 @@ import { Recipe } from "../../recipe/model/types";
 
 export interface CookingRecord extends Pick<Recipe, "name" | "ingredients"> {
   id: string;
+  createdAt: string; // ISO date string
+  updatedAt: string; // ISO date string
   cookedAt: string; // YYYY-MM-DD
 }
