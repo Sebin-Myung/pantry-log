e82c2e87039d556bb3963abcbc593287f8d277e5
Sebin-Myung <wrqewrq213@naver.com>
2026-02-04 22:23:31 +0900

    fix: useTextInput debounce 오류 롤백

--- shared_1/components/TextInput/model/useTextInput.ts
+++ shared_1/components/TextInput/model/useTextInput.ts
@@ -1,28 +1,12 @@
-import { debounce } from "@shared/lib";
-import { useEffect, useRef, useState } from "react";
-
 export interface IUseTextInput {
   value: string;
   setValue: (text: string) => void;
 }
 
 export function useTextInput({ value, setValue }: IUseTextInput) {
-  const [text, setText] = useState<string>(value);
-
-  const debouncedSetValue = useRef(
-    debounce((text: string) => {
-      setValue(text);
-    })
-  ).current;
-
   const onChangeText = (text: string) => {
-    setText(text);
-    debouncedSetValue(text);
+    setValue(text);
   };
 
-  useEffect(() => {
-    setText(value);
-  }, [value]);
-
-  return { value: text, onChangeText };
+  return { value, onChangeText };
 }
