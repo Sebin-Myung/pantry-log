7eaa88f24b50192651cd3933669238f4eb943b1e
Sebin-Myung <wrqewrq213@naver.com>
2026-02-10 15:43:00 +0900

    fix: EditCookingRecordForm prop type 수정 및 RecipeIngredients inputType 변경

--- widgets/cooking-record-form/ui/EditCookingRecordForm.tsx
+++ widgets/cooking-record-form/ui/EditCookingRecordForm.tsx
@@ -1,21 +1,24 @@
+import { CookingRecordSubmitItem } from "@entities";
 import { Label } from "@shared";
 import { RecipeIngredients } from "../../recipe-ingredients";
 import { IUseCookingRecordBaseForm } from "../model/types";
 import { useEditCookingRecordForm } from "../model/useEditCookingRecordForm";
 import { CookingRecordBaseForm } from "./CookingRecordBaseForm";
 
-export function EditCookingRecordForm({
-  initialState,
-  onSubmit,
-}: Pick<IUseCookingRecordBaseForm, "initialState" | "onSubmit">) {
+interface EditCookingRecordFormProps {
+  initialState: CookingRecordSubmitItem;
+  onSubmit: IUseCookingRecordBaseForm["onSubmit"];
+}
+
+export function EditCookingRecordForm({ initialState, onSubmit }: EditCookingRecordFormProps) {
   const { ingredients, setIngredients } = useEditCookingRecordForm({
-    initialState: { ingredients: initialState?.ingredients ?? [] },
+    initialState,
   });
 
   return (
     <CookingRecordBaseForm initialState={initialState} ingredients={ingredients} onSubmit={onSubmit}>
       <Label text="사용한 재료" required>
-        <RecipeIngredients inputType="dropdown" ingredients={ingredients} setIngredients={setIngredients} />
+        <RecipeIngredients inputType="input" ingredients={ingredients} setIngredients={setIngredients} />
       </Label>
     </CookingRecordBaseForm>
   );
