7961eadb63799d5a41d3fd5544bf7b2220f639a1
Sebin-Myung <wrqewrq213@naver.com>
2026-02-26 22:57:00 +0900

    fix: Switch 수직 정렬 오류 수정

--- shared/components/Switch/ui/Switch.tsx
+++ shared/components/Switch/ui/Switch.tsx
@@ -1,14 +1,16 @@
 import { useTheme } from "@shared";
-import { Switch as RNSwitch, SwitchProps } from "react-native";
+import { Switch as RNSwitch, SwitchProps, View } from "react-native";
 
 export function Switch({ trackColor, thumbColor, ...props }: SwitchProps) {
   const theme = useTheme();
 
   return (
-    <RNSwitch
-      trackColor={{ false: theme.colors.lightGray, true: theme.colors.primary, ...trackColor }}
-      thumbColor={thumbColor ?? theme.colors.white}
-      {...props}
-    />
+    <View>
+      <RNSwitch
+        trackColor={{ false: theme.colors.lightGray, true: theme.colors.primary, ...trackColor }}
+        thumbColor={thumbColor ?? theme.colors.white}
+        {...props}
+      />
+    </View>
   );
 }
